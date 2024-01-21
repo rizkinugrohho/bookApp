a42c3018801dc1f6cc97ea2aae21bcf1c4f3d050
Rizki Nugroho <rizkynhg@gmail.com>
2024-01-21 16:01:02 +0700

    Updated

---
+++ frontend/src/components/home/BooksTable.jsx
@@ -0,0 +1,83 @@
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+
+const BooksTable = ({ books }) => {
+  return (
+    <table className="w-full border-separate border-spacing-2">
+      <thead>
+        <tr>
+          <th className="border border-slate-600 rounded-md">No</th>
+          <th className="border border-slate-600 rounded-md">Title</th>
+          <th className="border border-slate-600 rounded-md">Description</th>
+          <th className="border border-slate-600 rounded-md max-md:hidden">
+            Image Url
+          </th>
+          <th className="border border-slate-600 rounded-md max-md:hidden">
+            Release
+          </th>
+          <th className="border border-slate-600 rounded-md">Price</th>
+          <th className="border border-slate-600 rounded-md max-md:hidden">
+            Total Page
+          </th>
+          <th className="border border-slate-600 rounded-md">Thickness</th>
+          <th className="border border-slate-600 rounded-md">Operations</th>
+        </tr>
+      </thead>
+      <tbody>
+        {books.map((book, index) => (
+          <tr key={book._id} className="h-8">
+            <td className="border border-slate-700 rounded-md text-center">
+              {index + 1}
+            </td>
+
+            <td className="border border-slate-700 rounded-md text-center">
+              {book.title}
+            </td>
+
+            <td className="border border-slate-700 rounded-md text-center">
+              {book.description}
+            </td>
+
+            <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+              {book.image_url}
+            </td>
+
+            <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+              {book.release_year}
+            </td>
+
+            <td className="border border-slate-700 rounded-md text-center">
+              {book.price}
+            </td>
+
+            <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+              {book.total_page}
+            </td>
+
+            <td className="border border-slate-700 rounded-md text-center">
+              {book.thickness}
+            </td>
+
+            <td className="border border-slate-700 rounded-md text-center[">
+              <div className="flex justify-center gap-x-4">
+                <Link to={`/books/details/${book._id}`}>
+                  <BsInfoCircle className="text-2x1 text-green-800" />
+                </Link>
+                <Link to={`/books/edit/${book._id}`}>
+                  <AiOutlineEdit className="text-2x1 text-yellow-600" />
+                </Link>
+                <Link to={`/books/delete/${book._id}`}>
+                  <MdOutlineDelete className="text-2x1 text-red-800" />
+                </Link>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default BooksTable;
