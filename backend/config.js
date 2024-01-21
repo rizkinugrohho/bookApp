a1bfa6ef6d7123f217b4637af57978170fa0e6b1
Rizki Nugroho <rizkynhg@gmail.com>
2024-01-21 09:36:33 +0700

    Updated

--- backend/config.js
+++
@@ -1,6 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL =
-  "mongodb+srv://rizkynhg:rizkyindah12@bookapp.fyx2vyc.mongodb.net/books-collection?retryWrites=true&w=majority";
-
-// Buat Database
